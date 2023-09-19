e8d1740e478d234dfaffa14516b30932f5a401b9
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-09-19 13:37:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
 
+const scroll = document.getElementById("infi-list");
+
+function isBottom(){
+	return scroll.scrollHeight - scroll.scrollTop == scroll.clientHeight;
+}
+
+function addMore() {
+	const ol = scroll;
+	for(let i =0;i<2;i++){
+		const li = document.createElement("li");
+		li.innerText = `Item ${ol.children.length+1}`;
+		ol.append(li);
+	}
+	
+}
+
+scroll.addEventListener("scroll",()=>{
+	if(isBottom()){
+		addMore();
+	}
+})
+addMore();
